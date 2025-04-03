734710e02e517d185280c4663f2602159167639f
Subiksha <subi12022006@gmail.com>
2025-04-04 00:21:30 +0530

    Updated all files

--- portfolio.js
+++
@@ -1,5 +0,0 @@
-function toggleImages() {
-    const hiddenImages = document.querySelectorAll('.hidden');
-    hiddenImages.forEach(img => img.classList.toggle('hidden'));
-    document.querySelector('.see-more-btn').style.display = 'none';
-}
